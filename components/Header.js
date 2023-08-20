b3bcd025d79af523e31521c356a42cbb0576dd5e
Khanh Nguyen <giakhanh006@gmail.com>
2023-08-20 12:54:14 +0700

    update mobile responsetive

--- components/Header.js
+++ components/Header.js
@@ -1,42 +1,17 @@
-import React from "react";
-import { useState, useEffect, useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Image from "next/image";
-import { FiArrowUpRight } from "react-icons/fi";
 import { AiOutlineDown } from "react-icons/ai";
+import { IoMdWallet } from "react-icons/io";
 import { HiOutlineDotsVertical } from "react-icons/hi";
 import ethLogo from "../asserts/eth.png";
-import uniLogo from "../asserts/ethCurrency.png";
 import uniswapLogo from "../asserts/uniswap.png";
-import {
-  TransactionContext,
-  TransactionProvider,
-} from "../context/TransactionContext";
-
-const style = {
-  wrapper: `p-4 w-screen flex justify-between items-center`,
-  headerLogo: `flex items-center justify-start`,
-  nav: `flex-1 flex justify-start items-center`,
-  navItemsContainer: `flex bg-[#191B1F] rounded-3xl`,
-  navItem: `px-4 py-2 m-1 flex items-center text-lg font-semibold text-[0.9rem] cursor-pointer rounded-3xl`,
-  activeNavItem: `bg-[#20242A]`,
-  buttonsContainer: `flex justify-end items-center`,
-  button: `flex items-center bg-[#191B1F] rounded-2xl mx-2 text-[0.9rem] font-semibold cursor-pointer`,
-  buttonPadding: `p-2`,
-  buttonTextContainer: `h-8 flex items-center`,
-  buttonIconContainer: `flex items-center justify-center w-8 h-8`,
-  buttonAccent: `bg-[#172A42] border border-[#163256] hover:border-[#234169] h-full rounded-2xl flex items-center justify-center text-[#4F90EA]`,
-};
+import { TransactionContext } from "../context/TransactionContext";
 
 const Header = () => {
-  const [selectedNav, setSelectedNav] = useState("swap"); //highlighted nav
+  const [selectedNav, setSelectedNav] = useState("swap");
+  const [showMoreOptions, setShowMoreOptions] = useState(false);
   const { connectWallet, currentAccount, balance, disconnectWallet } =
     useContext(TransactionContext);
-  //sd
-  const [showBalance, setShowBalance] = useState(0);
-
-  const toggleBalance = () => {
-    setShowBalance(!showBalance);
-  };
 
   useEffect(() => {
     if (currentAccount) {
@@ -46,7 +21,7 @@ const Header = () => {
 
   const getBalance = async (account) => {
     try {
-      const balanceInWei = await window.ethereum.request({
+      const balanceInWei = await ethereum.request({
         method: "eth_getBalance",
         params: [account, "latest"],
       });
@@ -58,94 +33,91 @@ const Header = () => {
   };
 
   return (
-    <div className={style.wrapper}>
-      <div className={style.headerLogo}>
+    <div className="p-4 w-screen flex flex-col md:flex-row justify-between items-center">
+      <div className="flex items-center justify-start md:w-1/4">
         <Image src={uniswapLogo} alt="uniswap" height={40} width={40} />{" "}
       </div>{" "}
-      <div className={style.nav}>
+      <div
+        className={`flex-1 flex flex-col md:flex-row justify-start items-center ${
+          showMoreOptions ? "block" : "hidden"
+        }`}
+      >
         <div
           onClick={() => setSelectedNav("swap")}
-          className={`${style.navItem} ${
-            selectedNav === "swap" && style.activeNavItem
+          className={`px-4 py-2 m-1 flex items-center text-lg font-semibold text-[0.9rem] cursor-pointer rounded-3xl ${
+            selectedNav === "swap" && "bg-[#20242A]"
           }`}
         >
           Swap{" "}
         </div>{" "}
         <div
-          onClick={() => setSelectedNav("pool")}
-          className={`${style.navItem} ${
-            selectedNav === "pool" && style.activeNavItem
-          }`}
-        >
-          Pool{" "}
-        </div>{" "}
-        <div
-          onClick={() => setSelectedNav("nft")}
-          className={`${style.navItem} ${
-            selectedNav === "nft" && style.activeNavItem
-          }`}
-        >
-          NFT{" "}
-        </div>{" "}
-        <div
           onClick={() => setSelectedNav("token")}
-          className={`${style.navItem} ${
-            selectedNav === "token" && style.activeNavItem
+          className={`px-4 py-2 m-1 flex items-center text-lg font-semibold text-[0.9rem] cursor-pointer rounded-3xl ${
+            selectedNav === "token" && "bg-[#20242A]"
           }`}
         >
           Token{" "}
         </div>{" "}
+        <div
+          onClick={() => setSelectedNav("nfts")}
+          className={`px-4 py-2 m-1 flex items-center text-lg font-semibold text-[0.9rem] cursor-pointer rounded-3xl ${
+            selectedNav === "nfts" && "bg-[#20242A]"
+          }`}
+        >
+          NFTs{" "}
+        </div>{" "}
+        <div
+          onClick={() => setSelectedNav("pools")}
+          className={`px-4 py-2 m-1 flex items-center text-lg font-semibold text-[0.9rem] cursor-pointer rounded-3xl ${
+            selectedNav === "pools" && "bg-[#20242A]"
+          }`}
+        >
+          Pools{" "}
+        </div>{" "}
       </div>{" "}
-      <div className={style.buttonsContainer}>
+      <div className="flex justify-end items-center">
         {" "}
         {currentAccount && (
-          <div className={`${style.button} ${style.buttonPadding}`}>
-            <div className={style.buttonTextContainer}>
+          <div className="flex items-center bg-[#191B1F] rounded-2xl mx-2 text-[0.9rem] font-semibold cursor-pointer p-2">
+            <div className="h-8 flex items-center">
               Balance: {balance}
               ETH{" "}
             </div>{" "}
           </div>
         )}{" "}
-        <div className={`${style.button} ${style.buttonPadding}`}>
-          <div className={style.buttonIconContainer}>
-            <Image src={ethLogo} alt="eth logo" height={20} width={20} />{" "}
-          </div>{" "}
-          <p> Ethereum </p>{" "}
-          <div className={style.buttonIconContainer}>
-            <AiOutlineDown />
-          </div>{" "}
-        </div>{" "}
         {currentAccount ? (
           <>
-            <div className={`${style.button} ${style.buttonPadding}`}>
-              <div className={style.buttonTextContainer}>
-                {" "}
-                {currentAccount}{" "}
+            <div className="flex items-center bg-[#191B1F] rounded-2xl mx-2 text-[0.9rem] font-semibold cursor-pointer p-2">
+              <div className="h-8 flex items-center">
+                <IoMdWallet />{" "}
+                {`${currentAccount.slice(0, 6)}...${currentAccount.slice(-4)}`}{" "}
               </div>{" "}
             </div>{" "}
             <div
               onClick={() => disconnectWallet()}
-              className={`${style.button} ${style.buttonPadding}`}
+              className="flex items-center bg-[#172A42] border border-[#163256] hover:border-[#234169] h-full rounded-2xl mx-2 text-[0.9rem] font-semibold cursor-pointer p-2"
             >
-              <div className={`${style.buttonAccent} ${style.buttonPadding}`}>
-                Disconnect Wallet{" "}
+              <div className="h-8 flex items-center justify-center text-[#4F90EA]">
+                Disconnect{" "}
               </div>{" "}
             </div>{" "}
           </>
         ) : (
           <div
             onClick={() => connectWallet()}
-            className={`${style.button} ${style.buttonPadding}`}
+            className="flex items-center bg-[#172A42] border border-[#163256] hover:border-[#234169] h-full rounded-2xl mx-2 text-[0.9rem] font-semibold cursor-pointer p-2"
           >
-            <div className={`${style.buttonAccent} ${style.buttonPadding}`}>
+            <div className="h-8 flex items-center justify-center text-[#4F90EA]">
               Connect Wallet{" "}
             </div>{" "}
           </div>
         )}{" "}
-        <div className={`${style.button} ${style.buttonPadding}`}>
-          <div className={`${style.buttonIconContainer} mx-2`}>
-            {" "}
-            {/* <HiOutLineDotsVertical /> */}{" "}
+        <div
+          className="flex items-center bg-[#191B1F] rounded-2xl mx-2 text-[0.9rem] font-semibold cursor-pointer p-2"
+          onClick={() => setShowMoreOptions(!showMoreOptions)}
+        >
+          <div className="h-8 flex items-center justify-center text-lg">
+            <HiOutlineDotsVertical />
           </div>{" "}
         </div>{" "}
       </div>{" "}
